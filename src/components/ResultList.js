81465d4896791c7dce95d2844def5d1d5d56c5b4
enesaliovur <61868498+enesaliovur@users.noreply.github.com>
2021-08-11 11:28:50 +0300

    Uygulama sisteme yüklendi

---
+++ src/components/ResultList.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
+import { ResultDetails } from "./ResultDetails";
+
+export const ResultList = ({ title, results, navigation }) => {
+
+  if(!results.length){
+    return null;
+  }
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
+      <FlatList
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        data={results}
+        keyExtractor={(result) => result.id}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity
+              onPress={() => navigation.navigate("ResultShow",{id:item.id})}
+            >
+              <ResultDetails result={item} />
+            </TouchableOpacity>
+          );
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 10,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginLeft: 15,
+    marginBottom: 5,
+  },
+});
